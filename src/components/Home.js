b0bb69c02978b88c8a86022feedbcd635d83d00a
edith.githinji <edithgithinji2020@gmail.com>
2025-01-17 08:24:49 +0300

    mpesa confirmation

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,8 +2,12 @@ import React from "react";
 import { name, city } from "../data/data.js";
 
 function Home() {
+ const h1 = `${name} is a Web Developer from ${city}`
   // update the JSX being returned!
-  return <div>Home</div>;
+  return (
+    <div id="home">
+       <h1 style={{color: "firebrick"}}> {h1} </h1>    
+    </div>);
 }
 
 export default Home;
